e890089242abd98ab22b05220aab83c69ccd5aae
Takayama Fumihiko <tekezo@pqrs.org>
2011-11-16 00:58:38 +0900

    Added app/XMLTest

---
+++ app/XMLTest/XMLTest/AppDelegate.h
@@ -0,0 +1,16 @@
+// -*- Mode: objc -*-
+//
+//  AppDelegate.h
+//  XMLTest
+//
+//  Created by Takayama Fumihiko on 11/11/16.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow* window;
+
+@end
---
+++ app/XMLTest/XMLTest/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  XMLTest
+//
+//  Created by Takayama Fumihiko on 11/11/16.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void) applicationDidFinishLaunching:(NSNotification*)aNotification
+{
+  NSError* error = nil;
+  NSString* xmlstring = [NSString stringWithContentsOfFile:@"/Library/org.pqrs/KeyRemap4MacBook/prefpane/checkbox.xml"
+                                                  encoding:NSUTF8StringEncoding
+                                                     error:&error];
+  NSLog(@"%@", xmlstring);
+}
+
+@end
