327194d4378c898657c2ebfbe3faca0ef249a00a
Takayama Fumihiko <tekezo@pqrs.org>
2012-03-17 20:48:12 +0900

    update test

--- code/tests/string/test.cpp
+++ code/tests/string/test.cpp
@@ -1,5 +1,6 @@
 #include <ostream>
 #include <gtest/gtest.h>
+#include <boost/lexical_cast.hpp>
 #include "pqrs/string.hpp"
 
 TEST(pqrs_string, string_by_replacing_double_curly_braces_from_file)
@@ -12,6 +13,15 @@ TEST(pqrs_string, string_by_replacing_double_curly_braces_from_file)
   std::string actual;
   pqrs::string::string_by_replacing_double_curly_braces_from_file(actual, "data/sample", replacement);
   EXPECT_EQ("1 2222 \n", actual);
+
+  // performance test
+  {
+    for (int i = 0; i < 1000; ++i) {
+      replacement[std::string("TARGET") + boost::lexical_cast<std::string>(i)] = "REPLACEMENT";
+    }
+    const char* filepath = "/Library/org.pqrs/KeyRemap4MacBook/prefpane/checkbox.xml";
+    pqrs::string::string_by_replacing_double_curly_braces_from_file(actual, filepath, replacement);
+  }
 }
 
 TEST(pqrs_string, string_by_replacing_double_curly_braces_from_string)
@@ -21,23 +31,36 @@ TEST(pqrs_string, string_by_replacing_double_curly_braces_from_string)
   replacement["BBB"] = "2222";
   replacement["CCC"] = "";
   replacement["DDD"] = "44444444444444444444";
+  replacement["LOOP1"] = "{{ LOOP1 }}";
+  replacement["LOOP2"] = "   {{ LOOP2 }}    ";
 
   std::string actual;
+
+  // no replacing
   pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "abc", replacement);
   EXPECT_EQ("abc", actual);
 
-  pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "{{AAA}} {{BBB}} {{ CCC }}", replacement);
-  EXPECT_EQ("1 2222 ", actual);
+  // normal replacing
+  pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "{{AAA}} {{BBB}} !{{ CCC }}!{{ DDD }}", replacement);
+  EXPECT_EQ("1 2222 !!44444444444444444444", actual);
 
+  // unknown replacing
+  pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "{{UNKNOWN}}", replacement);
+  EXPECT_EQ("", actual);
+
+  // "} }" is not end.
   pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "{{AAA} } BBB}} XXX", replacement);
   EXPECT_EQ(" XXX", actual);
 
+  // no }}
   pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "{{AAA}", replacement);
   EXPECT_EQ("{{AAA}", actual);
 
+  // no }}
   pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "{{ AAA }", replacement);
   EXPECT_EQ("{{ AAA }", actual);
 
-  pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "{{DDD}}", replacement);
-  EXPECT_EQ("44444444444444444444", actual);
+  // looped replacing
+  pqrs::string::string_by_replacing_double_curly_braces_from_string(actual, "{{ LOOP1 }}{{ LOOP2 }}", replacement);
+  EXPECT_EQ("{{ LOOP1 }}   {{ LOOP2 }}    ", actual);
 }
