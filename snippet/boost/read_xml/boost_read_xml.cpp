204cd60de0893b3894bbb8c9b897b5df36eca558
Takayama Fumihiko <tekezo@pqrs.org>
2012-03-18 11:17:57 +0900

    update snippet

--- snippet/boost/read_xml/boost_read_xml.cpp
+++ snippet/boost/read_xml/boost_read_xml.cpp
@@ -9,13 +9,24 @@ void
 read_xml_from_string(void)
 {
   boost::property_tree::ptree pt;
-  std::string xml = "<?xml version=\"1.0\"?><root><str>Hello &#8594; World</str></root>";
+  std::string xml = "<?xml version=\"1.0\"?>"
+    "<root>"
+    "<str id=\"str1\">Hello &#8594; World</str>"
+    "<str id=\"str2\">Hello2</str>"
+    "</root>";
   std::stringstream istream(xml, std::stringstream::in);
 
   boost::property_tree::read_xml(istream, pt);
 
   if (boost::optional<std::string> str = pt.get_optional<std::string>("root.str")) {
     std::cout << str.get() << std::endl;
+  }
+
+  BOOST_FOREACH (const boost::property_tree::ptree::value_type& child, pt.get_child("root")) {
+    std::cout << child.first.data() << " " << child.second.data() << std::endl;
+    if (boost::optional<std::string> id = child.second.get_optional<std::string>("<xmlattr>.id")) {
+      std::cout << "id: " << id.get() << std::endl;
+    }
   }
 }
 
