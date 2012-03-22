7bbf432de605cbfc3686c56e3161c089c33b9787
Takayama Fumihiko <tekezo@pqrs.org>
2012-03-22 18:21:45 +0900

    update code

--- code/pqrs/vector.cpp
+++ code/pqrs/vector.cpp
@@ -16,5 +16,37 @@ namespace pqrs {
     {
       std::copy(v2.begin(), v2.end(), std::back_inserter(v1));
     }
+
+    // ============================================================
+    static void
+    make_combination_(std::vector<std::tr1::shared_ptr<std::vector<std::string> > >& v,
+                      const std::vector<std::string>& seeds,
+                      size_t seeds_size)
+    {
+      typedef std::tr1::shared_ptr<std::vector<std::string> > ptr;
+
+      if (seeds_size == 0) {
+        ptr ptr(new std::vector<std::string>());
+        v.push_back(ptr);
+        return;
+      }
+
+      auto last = seeds[seeds_size - 1];
+      make_combination_(v, seeds, seeds_size - 1);
+      size_t size = v.size();
+      for (size_t i = 0; i < size; ++i) {
+        ptr copy(new std::vector<std::string>(*(v[i])));
+        copy->push_back(last);
+        v.push_back(copy);
+      }
+    }
+
+    void
+    make_combination(std::vector<std::tr1::shared_ptr<std::vector<std::string> > >& v,
+                     const std::vector<std::string>& seeds)
+    {
+      v.clear();
+      make_combination_(v, seeds, seeds.size());
+    }
   }
 }
--- code/tests/vector/test.cpp
+++ code/tests/vector/test.cpp
@@ -23,3 +23,113 @@ TEST(pqrs_vector, remove_empty_strings)
 
   EXPECT_EQ(expected, v);
 }
+
+TEST(pqrs_vector, make_combination)
+{
+  std::vector<std::string> seeds;
+  seeds.push_back("A");
+  seeds.push_back("B");
+  seeds.push_back("C");
+  seeds.push_back("D");
+  std::vector<std::tr1::shared_ptr<std::vector<std::string> > > actual;
+  pqrs::vector::make_combination(actual, seeds);
+
+  EXPECT_EQ(16, actual.size());
+
+  { // 0
+    std::vector<std::string> expected;
+    EXPECT_EQ(expected, *(actual[0]));
+  }
+  { // 1
+    std::vector<std::string> expected;
+    expected.push_back("A");
+    EXPECT_EQ(expected, *(actual[1]));
+  }
+  { // 2
+    std::vector<std::string> expected;
+    expected.push_back("B");
+    EXPECT_EQ(expected, *(actual[2]));
+  }
+  { // 3
+    std::vector<std::string> expected;
+    expected.push_back("A");
+    expected.push_back("B");
+    EXPECT_EQ(expected, *(actual[3]));
+  }
+  { // 4
+    std::vector<std::string> expected;
+    expected.push_back("C");
+    EXPECT_EQ(expected, *(actual[4]));
+  }
+  { // 5
+    std::vector<std::string> expected;
+    expected.push_back("A");
+    expected.push_back("C");
+    EXPECT_EQ(expected, *(actual[5]));
+  }
+  { // 6
+    std::vector<std::string> expected;
+    expected.push_back("B");
+    expected.push_back("C");
+    EXPECT_EQ(expected, *(actual[6]));
+  }
+  { // 7
+    std::vector<std::string> expected;
+    expected.push_back("A");
+    expected.push_back("B");
+    expected.push_back("C");
+    EXPECT_EQ(expected, *(actual[7]));
+  }
+  { // 8
+    std::vector<std::string> expected;
+    expected.push_back("D");
+    EXPECT_EQ(expected, *(actual[8]));
+  }
+  { // 9
+    std::vector<std::string> expected;
+    expected.push_back("A");
+    expected.push_back("D");
+    EXPECT_EQ(expected, *(actual[9]));
+  }
+  { // 10
+    std::vector<std::string> expected;
+    expected.push_back("B");
+    expected.push_back("D");
+    EXPECT_EQ(expected, *(actual[10]));
+  }
+  { // 11
+    std::vector<std::string> expected;
+    expected.push_back("A");
+    expected.push_back("B");
+    expected.push_back("D");
+    EXPECT_EQ(expected, *(actual[11]));
+  }
+  { // 12
+    std::vector<std::string> expected;
+    expected.push_back("C");
+    expected.push_back("D");
+    EXPECT_EQ(expected, *(actual[12]));
+  }
+  { // 13
+    std::vector<std::string> expected;
+    expected.push_back("A");
+    expected.push_back("C");
+    expected.push_back("D");
+    EXPECT_EQ(expected, *(actual[13]));
+  }
+  { // 14
+    std::vector<std::string> expected;
+    expected.push_back("B");
+    expected.push_back("C");
+    expected.push_back("D");
+    EXPECT_EQ(expected, *(actual[14]));
+  }
+  { // 15
+    std::vector<std::string> expected;
+    expected.push_back("A");
+    expected.push_back("B");
+    expected.push_back("C");
+    expected.push_back("D");
+    EXPECT_EQ(expected, *(actual[15]));
+  }
+}
