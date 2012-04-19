c28381ac42f458d74a5885d41eb26cb53ed7af19
Takayama Fumihiko <tekezo@pqrs.org>
2012-04-19 23:04:12 +0900

    update

--- script/dump_xml_compiler_result.sh
+++ script/dump_xml_compiler_result.sh
@@ -16,31 +16,26 @@ for command in \
     output_bridge_essential_configuration_default_values_c \
     output_bridge_essential_configuration_identifiers_m \
     ; do
-    echo "------------------------------------------------------------"
-    echo "$command old"
+    /bin/echo "${command}:"
+
+    ############################################################
+    /bin/echo -n "    old "
+
     /usr/bin/time /Library/org.pqrs/KeyRemap4MacBook/bin/dump_xml_compiler_result \
         $system_xml_directory "$private_xml_directory" $command > ~/$command.old
-    echo "------------------------------------------------------------"
 
-    echo "------------------------------------------------------------"
-    echo "$command new"
+    ############################################################
+    /bin/echo -n "    new "
+
     /usr/bin/time ../../KeyRemap4MacBook/src/bin/dump_xml_compiler_result/build/Release/dump_xml_compiler_result \
         $system_xml_directory "$private_xml_directory" $command > ~/$command.new
-    echo "------------------------------------------------------------"
 
-    echo "------------------------------------------------------------"
-    echo "$command devel"
+    ############################################################
+    /bin/echo -n "    dev "
+
     /usr/bin/time ../../KeyRemap4MacBook/src/bin/dump_xml_compiler_result/build/Release/dump_xml_compiler_result \
         $system_xml_directory_devel "$private_xml_directory" $command > ~/$command.devel
-    echo "------------------------------------------------------------"
 
-    echo "------------------------------------------------------------"
-    echo "diff old new"
     diff -u ~/$command.old ~/$command.new
-    echo "------------------------------------------------------------"
-
-    echo "------------------------------------------------------------"
-    echo "diff new devel"
     diff -u ~/$command.new ~/$command.devel
-    echo "------------------------------------------------------------"
 done
