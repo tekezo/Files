b557f19223c2a99967f39e1392912beda47c0fbc
Takayama Fumihiko <tekezo@pqrs.org>
2012-11-14 11:36:16 +0900

    update

--- app/TISTest/TISTest/AppDelegate.m
+++ app/TISTest/TISTest/AppDelegate.m
@@ -39,6 +39,28 @@
     TISInputSourceRef source = (TISInputSourceRef)(CFArrayGetValueAtIndex(list, i));
     if (! source) continue;
 
+    BOOL categoryfilter = NO;
+    NSString* category = TISGetInputSourceProperty(source, kTISPropertyInputSourceCategory);
+    if (! [category isEqualToString:(NSString*)(kTISCategoryKeyboardInputSource)]) {
+      categoryfilter = YES;
+    }
+    BOOL sourceidfilter = NO;
+    NSString* sourceID = TISGetInputSourceProperty(source, kTISPropertyInputSourceID);
+    if ([sourceID hasPrefix:@"com.apple."] &&
+        // com.apple.keylayout.French
+        ! [sourceID hasPrefix:@"com.apple.keylayout."] &&
+        // com.apple.inputmethod.Kotoeri.Roman
+        ! [sourceID hasPrefix:@"com.apple.inputmethod."] &&
+        // com.apple.keyboardlayout.fr-dvorak-bepo.keylayout.FrenchDvorak
+        ! [sourceID hasPrefix:@"com.apple.keyboardlayout."]) {
+      sourceidfilter = YES;
+    }
+
+    if (categoryfilter != sourceidfilter) {
+      NSLog(@"filter is not match");
+    }
+
+    NSLog(@"kTISPropertyInputSourceCategory: %@", TISGetInputSourceProperty(source, kTISPropertyInputSourceCategory));
     NSLog(@"kTISPropertyInputSourceID: %@", TISGetInputSourceProperty(source, kTISPropertyInputSourceID));
     NSLog(@"kTISPropertyInputModeID: %@", TISGetInputSourceProperty(source, kTISPropertyInputModeID));
 
