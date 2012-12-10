3ef30fb64aed6e10ef9037c60db83818c929d20f
Takayama Fumihiko <tekezo@pqrs.org>
2012-12-10 12:10:19 +0900

    update

--- private/KeyRemap4MacBook/exported.sh
+++ private/KeyRemap4MacBook/exported.sh
@@ -1,124 +1,245 @@
 #!/bin/sh
 
-cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+cli=/Users/tekezo/project/KeyRemap4MacBook/src/util/cli/build/Release/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
 $cli set remap.app_finder_disable_command_d 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlPNBF_eclipse 1
+/bin/echo -n .
 $cli set private.finderhack 1
+/bin/echo -n .
 $cli set remap.return2option 1
+/bin/echo -n .
 $cli set option.jis_drop_eisuukana_with_modifiers 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlH_x11 1
+/bin/echo -n .
 $cli set private.safari_option_d_to_command_l 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlM_term 1
+/bin/echo -n .
 $cli set private.language_switch 1
+/bin/echo -n .
 $cli set remap.drop_shiftL 1
+/bin/echo -n .
 $cli set private.app_powerpoint_hack 1
+/bin/echo -n .
 $cli set option.simultaneouskeypresses_vimode_f_to_fn 1
+/bin/echo -n .
 $cli set remap.simultaneouskeypresses_expose_kl 1
+/bin/echo -n .
 $cli set option.emacsmode_controlPNBF 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlLeftbracket_eclipse 1
+/bin/echo -n .
 $cli set private.browserhacks 1
+/bin/echo -n .
 $cli set option.emacsmode_controlH 1
+/bin/echo -n .
 $cli set option.emacsmode_controlV 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlI_eclipse 1
+/bin/echo -n .
 $cli set option.emacsmode_controlM 1
+/bin/echo -n .
 $cli set option.emacsmode_controlD 1
+/bin/echo -n .
 $cli set option.emacsmode_controlI 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlH_vm 1
+/bin/echo -n .
 $cli set remap.jis_eject2eisuuf13_withoutmodifiers 1
+/bin/echo -n .
 $cli set repeat.wait 30
+/bin/echo -n .
 $cli set remap.app_finder_disable_command_l 1
+/bin/echo -n .
 $cli set private.myexcelhacks 1
+/bin/echo -n .
 $cli set private.passthrough_notification 1
+/bin/echo -n .
 $cli set remap.space2shiftL_space_fnspace 1
+/bin/echo -n .
 $cli set private.vimode_s 1
+/bin/echo -n .
 $cli set option.pointing_enable_scrollwheel_fixation 1
+/bin/echo -n .
 $cli set remap.simultaneouskeypresses_vimode_sd_strict_key_order 1
+/bin/echo -n .
 $cli set private.pc_application_key_hack 1
+/bin/echo -n .
 $cli set remap.holding_command_num_to_function 1
+/bin/echo -n .
 $cli set remap.fn_scroll_to_brightness_control 1
+/bin/echo -n .
 $cli set option.mousekeys_mode_commandL2leftclick 1
+/bin/echo -n .
 $cli set private.mywordhack 1
+/bin/echo -n .
 $cli set remap.doublepresscommandQ 1
+/bin/echo -n .
 $cli set private.eject_to_sticky_extra1 1
+/bin/echo -n .
 $cli set remap.drop_shiftR 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlLeftbracket_vm 1
+/bin/echo -n .
 $cli set remap.pointing_strip_any 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlLeftbracket_term 1
+/bin/echo -n .
 $cli set remap.app_finder_disable_command_t 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlI_term 1
+/bin/echo -n .
 $cli set private.spaceshacks_with_swap_number_and_symbol 1
+/bin/echo -n .
 $cli set option.drop_slowexpose 1
+/bin/echo -n .
 $cli set private.app_office_hack 1
+/bin/echo -n .
 $cli set remap.enter2optionR 1
+/bin/echo -n .
 $cli set option.emacsmode_optionBF 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlM_x11 1
+/bin/echo -n .
 $cli set private.expose_hack 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlM_eclipse 1
+/bin/echo -n .
 $cli set remap.simultaneouskeypresses_mouse_keys_mode_ds_strict_key_order 1
+/bin/echo -n .
 $cli set remap.pointing_controlLeftclick2middleclick 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlI_vm 1
+/bin/echo -n .
 $cli set private.libreoffice_hack 1
+/bin/echo -n .
 $cli set private.vmhacks 1
+/bin/echo -n .
 $cli set option.simultaneouskeypresses_vimode_semicolonquote_to_optionleftright 1
+/bin/echo -n .
 $cli set option.mousekeys_mode_commandR2rightclick 1
+/bin/echo -n .
 $cli set private.pointing_leftclick2f13_trackpad 1
+/bin/echo -n .
 $cli set option.emacsmode_ex_controlU_delete 1
+/bin/echo -n .
 $cli set private.appswitchhacks 1
+/bin/echo -n .
 $cli set private.vimode_v 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlI_x11 1
+/bin/echo -n .
 $cli set remap.drop_controlL 1
+/bin/echo -n .
 $cli set remap.app_excel_command2_to_ctrlu 1
+/bin/echo -n .
 $cli set remap.swap_number_and_symbol 1
+/bin/echo -n .
 $cli set private.terminalhack 1
+/bin/echo -n .
 $cli set private.emacshack 1
+/bin/echo -n .
 $cli set private.space_to_tab_except_my_target_apps 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlH_eclipse 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlD_eclipse 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f10 1
+/bin/echo -n .
 $cli set option.emacsmode_optionD 1
+/bin/echo -n .
 $cli set private.rdchacks 1
+/bin/echo -n .
 $cli set parameter.statuswindow_fadeout_duration 1000
+/bin/echo -n .
 $cli set private.option_space_to_space_without_emacs 1
+/bin/echo -n .
 $cli set private.spaceshacks 1
+/bin/echo -n .
 $cli set remap.fn_i_to_toggle_overtype 1
+/bin/echo -n .
 $cli set remap.arrow2optionL 1
+/bin/echo -n .
 $cli set remap.app_skype_holdcommandW 1
+/bin/echo -n .
 $cli set remap.controlJ2return 1
+/bin/echo -n .
 $cli set private.limechathacks 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlM_vm 1
+/bin/echo -n .
 $cli set general.use_growl_instead_of_statuswindow 1
+/bin/echo -n .
 $cli set private.remap.pointing_leftclick2f12_trackpad 1
+/bin/echo -n .
 $cli set option.emacsmode_ex_controlX 1
+/bin/echo -n .
 $cli set option.emacsmode_controlAE_vm 1
+/bin/echo -n .
 $cli set remap.pc_application2commandL 1
+/bin/echo -n .
 $cli set option.emacsmode_ex_controlW 1
+/bin/echo -n .
 $cli set private.pointing_relative_rightclick_to_scroll 1
+/bin/echo -n .
 $cli set private.activitymonitor_hack 1
+/bin/echo -n .
 $cli set private.dropscrollwheel_0x05ac_0x030d 1
+/bin/echo -n .
 $cli set private.myword 1
+/bin/echo -n .
 $cli set parameter.wait_after_modifier 10
+/bin/echo -n .
 $cli set passthrough.escapetab 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlLeftbracket_x11 1
+/bin/echo -n .
 $cli set private.app_word_hack 1
+/bin/echo -n .
 $cli set private.jis_commandR2commandR_kana_return 1
+/bin/echo -n .
 $cli set private.eclipse 1
+/bin/echo -n .
 $cli set option.emacsmode_ex_control12 1
+/bin/echo -n .
 $cli set remap.delete2backquote 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f3_lion 1
+/bin/echo -n .
 $cli set private.controlscroll_to_controlarrow 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f7 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f5 1
+/bin/echo -n .
 $cli set option.emacsmode_ex_controlQ 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlPNBF_vm 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f1 1
+/bin/echo -n .
 $cli set option.mousekeys_mode_space2middleclick 1
+/bin/echo -n .
 $cli set private.jis_commandL2controlL_eisuu_escape 1
+/bin/echo -n .
 $cli set option.emacsmode_controlK_ex 1
+/bin/echo -n .
 $cli set option.expose_mode_f_to_optiontab 1
+/bin/echo -n .
 $cli set general.show_sticky_modifier_status 1
+/bin/echo -n .
 $cli set option.emacsmode_force_controlD_vm 1
+/bin/echo -n .
 $cli set remap.swiss_section_insert 1
+/bin/echo -n .
 $cli set option.emacsmode_controlLeftbracket 1
+/bin/echo -n .
 $cli set private.jis_oyayubishift_oldkana 1
+/bin/echo -n .
 $cli set private.jis_semicolon_roman 1000
+/bin/echo -n .
+/bin/echo
