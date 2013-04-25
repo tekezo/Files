9a10a9b59652c05fb62d92739d0c44eab3e8697c
Takayama Fumihiko <tekezo@pqrs.org>
2013-04-25 16:21:28 +0900

    Added EventTap

---
+++ app/EventTap/EventTap/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  EventTap
+//
+//  Created by Takayama Fumihiko on 2013/04/25.
+//  Copyright (c) 2013年 Takayama Fumihiko. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
