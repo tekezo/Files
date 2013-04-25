52d1109a6d12498b740c8be555c93d67d8a08a05
Takayama Fumihiko <tekezo@pqrs.org>
2013-04-25 16:21:28 +0900

    Added EventTap

---
+++ app/EventTap/EventTap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EventTap
+//
+//  Created by Takayama Fumihiko on 2013/04/25.
+//  Copyright (c) 2013年 Takayama Fumihiko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ app/EventTap/EventTap/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EventTap
+//
+//  Created by Takayama Fumihiko on 2013/04/25.
+//  Copyright (c) 2013年 Takayama Fumihiko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
