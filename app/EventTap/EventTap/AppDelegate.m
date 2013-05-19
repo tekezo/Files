32fb0b18b10b74e654f825dc61b0b1db24c235df
Takayama Fumihiko <tekezo@pqrs.org>
2013-05-19 10:22:08 +0900

    update

--- app/EventTap/EventTap/AppDelegate.m
+++ app/EventTap/EventTap/AppDelegate.m
@@ -9,45 +9,6 @@
 
 CFMachPortRef eventTap;
 
-enum {
-  POWER_KEY_TYPE_NONE,
-  POWER_KEY_TYPE_SUBTYPE, // subtype == NX_SUBTYPE_POWER_KEY
-  POWER_KEY_TYPE_KEYCODE, // subtype == NX_SUBTYPE_AUX_CONTROL_BUTTONS and keyCode == NX_POWER_KEY
-};
-
-// The power button sends two events.
-// POWER_KEY_TYPE_SUBTYPE and POWER_KEY_TYPE_KEYCODE.
-//
-// A build-in keyboard of MacBook sends these events.
-// - POWER_KEY_TYPE_SUBTYPE (at key down)
-// - POWER_KEY_TYPE_KEYCODE (at key down)
-//
-// An external keyboard which has power key sends these events.
-// - POWER_KEY_TYPE_SUBTYPE (at key down)
-// - POWER_KEY_TYPE_KEYCODE (at key down)
-// - POWER_KEY_TYPE_KEYCODE (at key up)
-
-+ (int) getPowerKeyType:(CGEventRef)cgEvent
-{
-  if (! cgEvent) return POWER_KEY_TYPE_NONE;
-
-  NSEvent* event = [NSEvent eventWithCGEvent:cgEvent];
-  if (! event) return POWER_KEY_TYPE_NONE;
-
-  if ([event type] != NSSystemDefined) return POWER_KEY_TYPE_NONE;
-
-  if ([event subtype] == NX_SUBTYPE_POWER_KEY) {
-    return POWER_KEY_TYPE_SUBTYPE;
-  }
-
-  int keyCode = (([event data1] & 0xFFFF0000) >> 16);
-  if (keyCode == NX_POWER_KEY) {
-    return POWER_KEY_TYPE_KEYCODE;
-  }
-
-  return POWER_KEY_TYPE_NONE;
-}
-
 CGEventRef eventTapCallBack(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void* refcon)
 {
   NSLog(@"eventTapCallBack: type:%d", type);
@@ -70,8 +31,6 @@ CGEventRef eventTapCallBack(CGEventTapProxy proxy, CGEventType type, CGEventRef 
 
 - (void) monitorPowerKey
 {
-  // We need to grab NSSystemDefined events.
-  // So, we call CGEventCreate with kCGEventMaskForAllEvents.
   eventTap = CGEventTapCreate(kCGSessionEventTap,
                               kCGHeadInsertEventTap,
                               kCGEventTapOptionDefault,
