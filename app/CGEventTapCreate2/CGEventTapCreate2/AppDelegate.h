ffeae8ac333fbb063ba011b577bc30ffad20b307
Takayama Fumihiko <tekezo@pqrs.org>
2013-05-24 18:03:31 +0900

    Added CGEventTapCreate2

---
+++ app/CGEventTapCreate2/CGEventTapCreate2/AppDelegate.h
@@ -0,0 +1,9 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow* window;
+
+@end
