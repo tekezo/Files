4131c74b504f17dbe18c0418ad8008cb5fbc292f
Takayama Fumihiko <tekezo@pqrs.org>
2013-05-24 18:03:31 +0900

    Added CGEventTapCreate2

---
+++ app/CGEventTapCreate2/CGEventTapCreate2/AppDelegate.m
@@ -0,0 +1,32 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+static CGEventRef eventTapCallBack(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void* refcon)
+{
+  return event;
+}
+
+- (void) applicationDidFinishLaunching:(NSNotification*)aNotification
+{
+  CFMachPortRef eventTap = CGEventTapCreate(kCGAnnotatedSessionEventTap,
+                                            kCGHeadInsertEventTap,
+                                            kCGEventTapOptionDefault,
+                                            kCGEventMaskForAllEvents,
+                                            eventTapCallBack,
+                                            NULL);
+  if (! eventTap) {
+    NSLog(@"CGEventTapCreate is failed");
+    return;
+  }
+
+  CFRunLoopSourceRef runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
+  CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
+
+  CGEventTapEnable(eventTap, 1);
+
+  CFRelease(runLoopSource);
+  CFRelease(eventTap);
+}
+
+@end
