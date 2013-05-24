e360de78609d8f810141aa9422136836c9b2d38d
Takayama Fumihiko <tekezo@pqrs.org>
2013-05-24 17:36:12 +0900

    Added CGEventTapCreate1

---
+++ app/CGEventTapCreate1/CGEventTapCreate1/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CGEventTapCreate1
+//
+//  Created by Takayama Fumihiko on 2013/05/24.
+//  Copyright (c) 2013年 Takayama Fumihiko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
