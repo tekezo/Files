541d5d1aeb9c2cf3fa0146613e225c1e75af3d6d
Takayama Fumihiko <tekezo@pqrs.org>
2014-03-11 19:45:32 +0900

    add app

--- app/MultiDisplays/MultiDisplays/AppDelegate.m
+++ app/MultiDisplays/MultiDisplays/AppDelegate.m
@@ -4,15 +4,11 @@
 
 - (void) applicationDidFinishLaunching:(NSNotification*)aNotification
 {
-  NSWindowCollectionBehavior behavior = NSWindowCollectionBehaviorCanJoinAllSpaces |
-                                        NSWindowCollectionBehaviorStationary |
-                                        NSWindowCollectionBehaviorIgnoresCycle;
-  [self.window setLevel:NSStatusWindowLevel];
-  [self.window setCollectionBehavior:behavior];
+  [self.window setCollectionBehavior:NSWindowCollectionBehaviorStationary];
 
   for (NSScreen* screen in [NSScreen screens]) {
     // BUG! self.window will be vanished after this orderOut even if we call orderFront.
-    //[self.window orderOut:self];
+    [self.window orderOut:self];
     [self.window setFrameOrigin:[screen frame].origin];
     [self.window orderFront:self];
   }
