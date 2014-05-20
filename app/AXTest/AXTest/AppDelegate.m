3bae25222a5e59d4d6807e28e1d51a153476aef3
Takayama Fumihiko <tekezo@pqrs.org>
2014-05-20 18:19:43 +0900

    update AXTest

--- app/AXTest/AXTest/AppDelegate.m
+++ app/AXTest/AXTest/AppDelegate.m
@@ -1,16 +1,67 @@
 #import "AppDelegate.h"
+#import "AXUtilities.h"
 
 @implementation AppDelegate
+
+static void observerCallback(AXObserverRef observer, AXUIElementRef element, CFStringRef notification, void* refcon)
+{
+  NSLog(@"observerCallback %@", (__bridge NSString*)(notification));
+
+  if (CFStringCompare(notification, kAXTitleChangedNotification, 0) == kCFCompareEqualTo) {
+    NSLog(@"title changed: %@", [AXUtilities titleOfUIElement:element]);
+  } else if (CFStringCompare(notification, kAXFocusedWindowChangedNotification, 0) == kCFCompareEqualTo) {
+    NSLog(@"focused window changed: %@", [AXUtilities titleOfUIElement:element]);
+  }
+}
+
+- (void) registerApplication:(NSRunningApplication*)runningApplication
+{
+  pid_t processIdentifier = [runningApplication processIdentifier];
+
+  AXUIElementRef application = AXUIElementCreateApplication(processIdentifier);
+
+  AXObserverRef observer;
+  AXObserverCreate(processIdentifier, observerCallback, &observer);
+
+  AXError error = kAXErrorSuccess;
+  error = AXObserverAddNotification(observer, application, kAXTitleChangedNotification,         (__bridge void*)self);
+  error = AXObserverAddNotification(observer, application, kAXWindowCreatedNotification,        (__bridge void*)self);
+  error = AXObserverAddNotification(observer, application, kAXFocusedWindowChangedNotification, (__bridge void*)self);
+  error = AXObserverAddNotification(observer, application, kAXFocusedUIElementChangedNotification, (__bridge void*)self);
+
+  CFRunLoopAddSource([[NSRunLoop currentRunLoop] getCFRunLoop],
+                     AXObserverGetRunLoopSource(observer),
+                     kCFRunLoopDefaultMode);
+}
+
+- (void) observer_NSWorkspaceDidLaunchApplicationNotification:(NSNotification*)notification
+{
+  NSLog(@"observer_NSWorkspaceDidLaunchApplicationNotification");
+
+  NSRunningApplication* runningApplication = [notification userInfo][NSWorkspaceApplicationKey];
+  [self registerApplication:runningApplication];
+}
 
 - (void) applicationDidFinishLaunching:(NSNotification*)aNotification
 {
   NSDictionary* options = @{ (__bridge NSString*)(kAXTrustedCheckOptionPrompt): @YES };
   BOOL accessibilityEnabled = AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)options);
 
-  while (true) {
-    [NSThread sleepForTimeInterval:1];
-    NSLog(@"%@", [NSApp accessibilityFocusedUIElement]);
+  if (! accessibilityEnabled) {
+    [[NSAlert alertWithError:@"Please permit the accessibility features."] runModal];
+
+  } else {
+    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+                                                           selector:@selector(observer_NSWorkspaceDidLaunchApplicationNotification:)
+                                                               name:NSWorkspaceDidLaunchApplicationNotification
+                                                             object:nil];
+
+    for (NSRunningApplication* runningApplication in [[NSWorkspace sharedWorkspace] runningApplications]) {
+      [self registerApplication:runningApplication];
+    }
   }
 }
+
+// call AXObserverRemoveNotification
 
 @end
---
+++ app/AXTest/AXTest/Classes/AXUtilities.h
@@ -0,0 +1,9 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+@import Cocoa;
+
+@interface AXUtilities : NSObject
+
++ (NSString*) titleOfUIElement:(AXUIElementRef)element;
+
+@end
