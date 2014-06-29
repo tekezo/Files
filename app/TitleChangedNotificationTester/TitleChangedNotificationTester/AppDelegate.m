c0ef3dd2b4e75d2c43865513d0dcd9327f900f9c
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-29 15:01:03 +0900

    update

---
+++ app/TitleChangedNotificationTester/TitleChangedNotificationTester/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  TitleChangedNotificationTester
+//
+//  Created by Takayama Fumihiko on 2014/06/29.
+//  Copyright (c) 2014年 Takayama Fumihiko. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
