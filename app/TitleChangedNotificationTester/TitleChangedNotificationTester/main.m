2acb8ace24c3fa9cc40fd16b16fdf7c8e92db5f6
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-29 15:05:05 +0900

    update

--- app/TitleChangedNotificationTester/TitleChangedNotificationTester/main.m
+++ app/TitleChangedNotificationTester/TitleChangedNotificationTester/main.m
@@ -1,14 +1,6 @@
-//
-//  main.m
-//  TitleChangedNotificationTester
-//
-//  Created by Takayama Fumihiko on 2014/06/29.
-//  Copyright (c) 2014年 Takayama Fumihiko. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, const char * argv[])
+int main(int argc, const char* argv[])
 {
-    return NSApplicationMain(argc, argv);
+  return NSApplicationMain(argc, argv);
 }
