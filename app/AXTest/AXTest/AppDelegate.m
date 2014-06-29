c97f11913ee5286b00f45009dd7af97f51f0bebb
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-30 00:31:27 +0900

    update

--- app/AXTest/AXTest/AppDelegate.m
+++ app/AXTest/AXTest/AppDelegate.m
@@ -1,4 +1,4 @@
-#import "AXApplicationObserver.h"
+#import "AXApplicationObserverManager.h"
 #import "AXUtilities.h"
 #import "AppDelegate.h"
 #import "NotificationKeys.h"
@@ -6,51 +6,12 @@
 
 @interface AppDelegate ()
 {
-  NSMutableDictionary* observers_;
+  AXApplicationObserverManager* axApplicationObserverManager_;
   WindowObserver* windowObserver_;
-  NSTimer* timer_;
-  NSRunningApplication* runningApplicationForAXApplicationObserver_;
 }
 @end
 
 @implementation AppDelegate
-
-- (void) timerFireMethod:(NSTimer*)timer
-{
-  dispatch_async(dispatch_get_main_queue(), ^{
-    @synchronized(self) {
-      if (runningApplicationForAXApplicationObserver_) {
-        @try {
-          AXApplicationObserver* o = [[AXApplicationObserver alloc] initWithRunningApplication:runningApplicationForAXApplicationObserver_];
-          pid_t pid = [runningApplicationForAXApplicationObserver_ processIdentifier];
-          observers_[@(pid)] = o;
-
-          [o observeTitleChangedNotification];
-          [o postNotification];
-
-          runningApplicationForAXApplicationObserver_ = nil;
-
-        } @catch (NSException* e) {
-          NSLog(@"%@", e);
-        }
-      }
-    }
-  });
-}
-
-- (void) observer_NSWorkspaceDidActivateApplicationNotification:(NSNotification*)notification
-{
-  dispatch_async(dispatch_get_main_queue(), ^{
-    @synchronized(self) {
-      for (NSNumber* pid in observers_) {
-        [observers_[pid] unobserveTitleChangedNotification];
-      }
-
-      runningApplicationForAXApplicationObserver_ = [notification userInfo][NSWorkspaceApplicationKey];
-      [timer_ fire];
-    }
-  });
-}
 
 - (void) observer_kFocusedUIElementChanged:(NSNotification*)notification
 {
@@ -64,9 +25,6 @@
 
 - (void) applicationDidFinishLaunching:(NSNotification*)aNotification
 {
-  observers_ = [NSMutableDictionary new];
-  windowObserver_ = [WindowObserver new];
-
   NSDictionary* options = @{ (__bridge NSString*)(kAXTrustedCheckOptionPrompt): @YES };
   BOOL accessibilityEnabled = AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)options);
 
@@ -78,11 +36,7 @@
          informativeTextWithFormat:@""] runModal];
   }
 
-  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
-                                                         selector:@selector(observer_NSWorkspaceDidActivateApplicationNotification:)
-                                                             name:NSWorkspaceDidActivateApplicationNotification
-                                                           object:nil];
-
+  // ----------------------------------------
   [[NSNotificationCenter defaultCenter] addObserver:self
                                            selector:@selector(observer_kFocusedUIElementChanged:)
                                                name:kFocusedUIElementChanged
@@ -93,23 +47,8 @@
                                                name:kWindowVisibilityChanged
                                              object:nil];
 
-  for (NSRunningApplication* runningApplication in [[NSWorkspace sharedWorkspace] runningApplications]) {
-    @try {
-      pid_t pid = [runningApplication processIdentifier];
-      AXApplicationObserver* app = [[AXApplicationObserver alloc] initWithRunningApplication:runningApplication];
-      observers_[@(pid)] = app;
-    } @catch (NSException* e) {
-      NSLog(@"%@", e);
-    }
-  }
-
-  timer_ = [NSTimer scheduledTimerWithTimeInterval:0.5
-                                            target:self
-                                          selector:@selector(timerFireMethod:)
-                                          userInfo:nil
-                                           repeats:YES];
-  runningApplicationForAXApplicationObserver_ = [[NSWorkspace sharedWorkspace] frontmostApplication];
-  [timer_ fire];
+  axApplicationObserverManager_ = [AXApplicationObserverManager new];
+  windowObserver_ = [WindowObserver new];
 }
 
 @end
