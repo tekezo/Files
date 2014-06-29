b419ae362f9102e6320383cb2a0e0305cb55d157
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-29 15:18:36 +0900

    update

--- app/TitleChangedNotificationTester/TitleChangedNotificationTester/AppDelegate.m
+++ app/TitleChangedNotificationTester/TitleChangedNotificationTester/AppDelegate.m
@@ -16,13 +16,16 @@
   NSRect rect = [[NSScreen mainScreen] frame];
 
   [self.window1 setTitle:@"window1"];
+  [self.window1 setLevel:NSFloatingWindowLevel];
   [self.window1 setFrameTopLeftPoint:NSMakePoint(100, rect.size.height - 100)];
 
   [self.window2 setTitle:@"window2"];
-  [self.window2 setFrameTopLeftPoint:NSMakePoint(200, rect.size.height - 200)];
+  [self.window2 setLevel:NSFloatingWindowLevel];
+  [self.window2 setFrameTopLeftPoint:NSMakePoint(200, rect.size.height - 400)];
 
   [self.window3 setTitle:@"window3"];
-  [self.window3 setFrameTopLeftPoint:NSMakePoint(300, rect.size.height - 300)];
+  [self.window3 setLevel:NSFloatingWindowLevel];
+  [self.window3 setFrameTopLeftPoint:NSMakePoint(300, rect.size.height - 700)];
 
   [NSTimer scheduledTimerWithTimeInterval:0.5
                                    target:self
