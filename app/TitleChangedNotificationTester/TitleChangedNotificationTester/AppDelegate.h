fab262798fa5347d7ce9ce3a17494a5a25956e13
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-29 15:05:05 +0900

    update

--- app/TitleChangedNotificationTester/TitleChangedNotificationTester/AppDelegate.h
+++ app/TitleChangedNotificationTester/TitleChangedNotificationTester/AppDelegate.h
@@ -1,15 +1,9 @@
-//
-//  AppDelegate.h
-//  TitleChangedNotificationTester
-//
-//  Created by Takayama Fumihiko on 2014/06/29.
-//  Copyright (c) 2014年 Takayama Fumihiko. All rights reserved.
-//
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
-@property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSWindow* window;
 
 @end
