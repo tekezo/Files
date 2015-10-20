1844f43abd0df5c7d109d1aaab85f3c74744e28b
Takayama Fumihiko <tekezo@pqrs.org>
2015-10-20 21:51:30 +0900

    add testapp

---
+++ app/testapp/testapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testapp
+//
+//  Created by Takayama Fumihiko on 10/20/15.
+//  Copyright © 2015 Takayama Fumihiko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ app/testapp/testapp/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  testapp
+//
+//  Created by Takayama Fumihiko on 10/20/15.
+//  Copyright © 2015 Takayama Fumihiko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
