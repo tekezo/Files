dcac098a5a81738b89e718572230fe9e2699fb05
Takayama Fumihiko <tekezo@pqrs.org>
2015-10-31 00:51:07 +0900

    update

--- app/testapp/testapp/AppDelegate.m
+++ app/testapp/testapp/AppDelegate.m
@@ -1,29 +1,34 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate () {
-  NSTimer *timer_;
+  NSTimer* timer_;
 }
 
-@property(weak) IBOutlet NSWindow *window;
+@property(weak) IBOutlet NSWindow* window;
 @end
 
 @implementation AppDelegate
 
-- (void)timerFireMethod:(NSTimer *)timer {
-  NSDictionary *defaults = [[NSUserDefaults standardUserDefaults] persistentDomainForName:NSGlobalDomain];
+- (void)timerFireMethod:(NSTimer*)timer {
+  NSDictionary* defaults = [[NSUserDefaults standardUserDefaults] persistentDomainForName:NSGlobalDomain];
   NSLog(@"%@", defaults[@"InitialKeyRepeat"]);
   NSLog(@"%@", defaults[@"KeyRepeat"]);
 }
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+- (void)applicationDidFinishLaunching:(NSNotification*)aNotification {
   timer_ = [NSTimer scheduledTimerWithTimeInterval:1
                                             target:self
                                           selector:@selector(timerFireMethod:)
                                           userInfo:nil
                                            repeats:YES];
+
+  NSNumberFormatter* f = [NSNumberFormatter new];
+  f.numberStyle = NSNumberFormatterDecimalStyle;
+  NSLog(@"%@", [f numberFromString:@"12,345.6"]);
+  NSLog(@"%@", [f numberFromString:@"1234,5.6"]);
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
+- (void)applicationWillTerminate:(NSNotification*)aNotification {
   // Insert code here to tear down your application
 }
 
