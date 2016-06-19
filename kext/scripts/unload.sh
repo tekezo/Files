56322cd19d833d9788c4c9356fd4a1ab5a32fa80
Takayama Fumihiko <tekezo@pqrs.org>
2016-06-19 11:41:41 +0900

    update kext

--- kext/scripts/unload.sh
+++ kext/scripts/unload.sh
@@ -3,6 +3,6 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
 
 # --------------------------------------------------
-sudo kextunload -b org.pqrs.mykext
+sudo kextunload -b org.pqrs.driver.mykext
 
 exit 0
