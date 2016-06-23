1a257a46cc7b6d15559035d319a6526e2a3651d2
Takayama Fumihiko <tekezo@pqrs.org>
2016-06-23 22:16:32 +0900

    update kext

--- kext/IOHIDEventService/mykext/mykext.hpp
+++ kext/IOHIDEventService/mykext/mykext.hpp
@@ -2,34 +2,12 @@
 
 #include "diagnostic_macros.hpp"
 
-#define private public /* A hack for access private member of IOHIKeyboard */
-#define protected public /* A hack for access private member of IOHIKeyboard */
 BEGIN_IOKIT_INCLUDE;
 #include <IOKit/IOLib.h>
 #include <IOKit/IOService.h>
-#include <IOKit/hidevent/IOHIDEventDriver.h>
-#include <IOKit/hidsystem/IOHIDUsageTables.h>
-#include <IOKit/hidsystem/IOHIKeyboard.h>
-#include <IOKit/hidsystem/IOHIPointing.h>
-#include "IOHIDKeyboard.h"
+#include <IOKit/hidevent/IOHIDEventService.h>
 END_IOKIT_INCLUDE;
-#undef protected
-#undef private
 
-class org_pqrs_driver_mykext : public IOService {
+class org_pqrs_driver_mykext : public IOHIDEventService {
   OSDeclareDefaultStructors(org_pqrs_driver_mykext);
-
-public:
-  virtual bool init(OSDictionary* dictionary = 0) override;
-  virtual void free(void) override;
-  virtual IOService* probe(IOService* provider, SInt32* score) override;
-  virtual bool start(IOService* provider) override;
-  virtual void stop(IOService* provider) override;
-
-  static bool gIOMatchedNotification_callback(void* target, void* refCon, IOService* newService, IONotifier* notifier);
-  static bool gIOTerminatedNotification_callback(void* target, void* refCon, IOService* newService, IONotifier* notifier);
-
-private:
-  IONotifier* matchedNotifier_;
-  IONotifier* terminatedNotifier_;
 };
