e2e5a1bc9469ea64571fd2ef8e5415e1064fb805
Takayama Fumihiko <tekezo@pqrs.org>
2016-06-25 00:11:09 +0900

    update kext

--- kext/IOHIDEventDriver/mykext/mykext.hpp
+++ kext/IOHIDEventDriver/mykext/mykext.hpp
@@ -11,4 +11,17 @@ class org_pqrs_driver_mykext : public IOHIDEventDriver {
 
 public:
   virtual bool start(IOService* provider) override;
+
+  virtual void dispatchKeyboardEvent(
+      AbsoluteTime timeStamp,
+      UInt32 usagePage,
+      UInt32 usage,
+      UInt32 value,
+      IOOptionBits options = 0) override;
+
+  virtual void handleInterruptReport(
+      AbsoluteTime timeStamp,
+      IOMemoryDescriptor* report,
+      IOHIDReportType reportType,
+      UInt32 reportID) override;
 };
