1d01676fc0232ce0e84d1a691995e85296a2ffc5
Takayama Fumihiko <tekezo@pqrs.org>
2016-06-26 11:31:56 +0900

    add EventTapDev

---
+++ app/EventTapDev/EventTapDev/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EventTapDev
+//
+//  Created by Takayama Fumihiko on 6/26/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
