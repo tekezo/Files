10d2b28758fb5efb5484cd2ba821bf660b08fc1e
Takayama Fumihiko <tekezo@pqrs.org>
2016-06-28 11:26:10 +0900

    update

--- bin/eventhook/eventhook/main.cpp
+++ bin/eventhook/eventhook/main.cpp
@@ -1,6 +1,52 @@
+#include <CoreGraphics/CoreGraphics.h>
 #include <iostream>
 
+class EventHook {
+public:
+  EventHook(void) {
+    eventTap_ = CGEventTapCreate(kCGHIDEventTap,
+                                 kCGHeadInsertEventTap,
+                                 kCGEventTapOptionDefault,
+                                 CGEventMaskBit(kCGEventKeyDown) | CGEventMaskBit(kCGEventKeyUp),
+                                 EventHook::eventTapCallBack,
+                                 NULL);
+    if (!eventTap_) {
+      std::cerr << "CGEventTapCreate is failed" << std::endl;
+      return;
+    }
+
+    CFRunLoopSourceRef runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap_, 0);
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
+
+    CGEventTapEnable(eventTap_, 1);
+
+    CFRelease(runLoopSource);
+  }
+
+  static CGEventRef eventTapCallBack(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void* refcon) {
+    std::cout << "type " << type << std::endl;
+    switch (type) {
+    case kCGEventKeyDown:
+    case kCGEventKeyUp: {
+      int64_t keycode = CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+      std::cout << "key " << keycode;
+      // Escape
+      if (keycode == 0x35) {
+      }
+      break;
+    }
+    default:
+      break;
+    }
+    return event;
+  }
+
+private:
+  CFMachPortRef eventTap_;
+};
+
 int main(int argc, const char* argv[]) {
-  std::cout << "Hello, World!\n";
+  EventHook hook;
+  CFRunLoopRun();
   return 0;
 }
