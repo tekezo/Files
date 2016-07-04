2f6724b3bc4a2e480083c54ff0676ae4a89c4cb9
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-05 00:24:00 +0900

    add event_grabber

---
+++ bin/eventhook/eventhook/include/event_grabber.hpp
@@ -0,0 +1,187 @@
+#pragma once
+
+class event_grabber final {
+public:
+  event_grabber(void) {
+    manager_ = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
+    if (!manager_) {
+      return;
+    }
+
+    auto device_matching_dictionaries = create_device_matching_dictionaries();
+    if (device_matching_dictionaries) {
+      IOHIDManagerSetDeviceMatchingMultiple(manager_, device_matching_dictionaries);
+      CFRelease(device_matching_dictionaries);
+
+      IOHIDManagerRegisterDeviceMatchingCallback(manager_, device_matching_callback, this);
+
+      IOHIDManagerScheduleWithRunLoop(manager_, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+
+      IOReturn result = IOHIDManagerOpen(manager_, kIOHIDOptionsTypeSeizeDevice);
+      if (result == kIOReturnSuccess) {
+        std::cout << "opened" << std::endl;
+        IOHIDManagerRegisterInputValueCallback(manager_, inputValueCallback, this);
+      }
+    }
+  }
+
+  ~event_grabber(void) {
+    if (manager_) {
+      IOHIDManagerUnscheduleFromRunLoop(manager_, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+      CFRelease(manager_);
+      manager_ = nullptr;
+    }
+  }
+
+private:
+  CFDictionaryRef _Nonnull create_device_matching_dictionary(uint32_t usage_page, uint32_t usage) {
+    auto device_matching_dictionary = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    if (!device_matching_dictionary) {
+      goto finish;
+    }
+
+    // usage_page
+    if (!usage_page) {
+      goto finish;
+    } else {
+      auto number = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &usage_page);
+      if (!number) {
+        goto finish;
+      }
+      CFDictionarySetValue(device_matching_dictionary, CFSTR(kIOHIDDeviceUsagePageKey), number);
+      CFRelease(number);
+    }
+
+    // usage (The usage is only valid if the usage page is also defined)
+    if (!usage) {
+      goto finish;
+    } else {
+      auto number = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &usage);
+      if (!number) {
+        goto finish;
+      }
+      CFDictionarySetValue(device_matching_dictionary, CFSTR(kIOHIDDeviceUsageKey), number);
+      CFRelease(number);
+    }
+
+  finish:
+    return device_matching_dictionary;
+  }
+
+  CFArrayRef _Nullable create_device_matching_dictionaries(void) {
+    auto device_matching_dictionaries = CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks);
+    if (!device_matching_dictionaries) {
+      return nullptr;
+    }
+
+    // kHIDUsage_GD_Keyboard
+    {
+      auto device_matching_dictionary = create_device_matching_dictionary(kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard);
+      if (device_matching_dictionary) {
+        CFArrayAppendValue(device_matching_dictionaries, device_matching_dictionary);
+        CFRelease(device_matching_dictionary);
+      }
+    }
+
+    // kHIDUsage_GD_Mouse
+    {
+      auto device_matching_dictionary = create_device_matching_dictionary(kHIDPage_GenericDesktop, kHIDUsage_GD_Mouse);
+      if (device_matching_dictionary) {
+        CFArrayAppendValue(device_matching_dictionaries, device_matching_dictionary);
+        CFRelease(device_matching_dictionary);
+      }
+    }
+
+    return device_matching_dictionaries;
+  }
+
+  static void device_matching_callback(void* _Nullable context, IOReturn result, void* _Nullable sender, IOHIDDeviceRef _Nonnull device) {
+    if (result != kIOReturnSuccess) {
+      return;
+    }
+
+    auto self = static_cast<event_grabber*>(context);
+    if (!self) {
+      return;
+    }
+
+    if (!device) {
+      return;
+    }
+
+    auto vendor_id = self->get_vendor_id(device);
+    auto product_id = self->get_product_id(device);
+
+    std::cout << "vendor_id:0x" << std::hex << vendor_id << " product_id:0x" << std::hex << product_id << std::endl;
+  }
+
+  bool get_long_property(const IOHIDDeviceRef _Nonnull device, const CFStringRef _Nonnull key, long& value) {
+    if (!device) {
+      return false;
+    }
+
+    auto property = IOHIDDeviceGetProperty(device, key);
+    if (!property) {
+      return false;
+    }
+
+    if (CFNumberGetTypeID() != CFGetTypeID(property)) {
+      return false;
+    }
+
+    return CFNumberGetValue(static_cast<CFNumberRef>(property), kCFNumberLongType, &value);
+  }
+
+  long get_vendor_id(IOHIDDeviceRef _Nonnull device) {
+    long value = 0;
+    get_long_property(device, CFSTR(kIOHIDVendorIDKey), value);
+    return value;
+  }
+
+  long get_product_id(IOHIDDeviceRef _Nonnull device) {
+    long value = 0;
+    get_long_property(device, CFSTR(kIOHIDProductIDKey), value);
+    return value;
+  }
+
+  static void inputValueCallback(
+      void* _Nullable context,
+      IOReturn result,
+      void* _Nullable sender,
+      IOHIDValueRef _Nullable value) {
+    if (!context) {
+      return;
+    }
+
+    // auto self = static_cast<event_grabber*>(context);
+
+    if (value) {
+      auto element = IOHIDValueGetElement(value);
+      auto usagePage = IOHIDElementGetUsagePage(element);
+      auto usage = IOHIDElementGetUsage(element);
+      auto integerValue = IOHIDValueGetIntegerValue(value);
+
+      switch (usagePage) {
+      case kHIDPage_KeyboardOrKeypad:
+        if (usage == kHIDUsage_KeyboardErrorRollOver ||
+            usage == kHIDUsage_KeyboardPOSTFail ||
+            usage == kHIDUsage_KeyboardErrorUndefined ||
+            usage >= kHIDUsage_GD_Reserved) {
+          // do nothing
+        } else {
+          // bool keyDown = (integerValue == 1);
+          std::cout << "inputValueCallback usagePage:" << usagePage << " usage:" << usage << " value:" << integerValue << std::endl;
+          if (usage == kHIDUsage_KeyboardEscape) {
+            exit(0);
+          }
+        }
+        break;
+
+      default:
+        std::cout << "inputValueCallback unknown usagePage:" << usagePage << " usage:" << usage << std::endl;
+      }
+    }
+  }
+
+  IOHIDManagerRef _Nullable manager_;
+};
--- bin/eventhook/eventhook/main.cpp
+++ bin/eventhook/eventhook/main.cpp
@@ -6,6 +6,8 @@
 #include <IOKit/hidsystem/ev_keymap.h>
 #include <iostream>
 
+#include "event_grabber.hpp"
+
 class IOHIDPostEventWrapper final {
 public:
   IOHIDPostEventWrapper(void) : eventDriver_(IO_OBJECT_NULL) {
@@ -87,167 +89,6 @@ private:
   mach_port_t eventDriver_;
 };
 
-class IOHIDManagerObserver final {
-public:
-  IOHIDManagerObserver(void) {
-    manager_ = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
-    if (manager_) {
-      auto deviceMatchingDictionaryArray = createDeviceMatchingDictionaryArray();
-      if (deviceMatchingDictionaryArray) {
-        IOHIDManagerSetDeviceMatchingMultiple(manager_, deviceMatchingDictionaryArray);
-        CFRelease(deviceMatchingDictionaryArray);
-
-        IOHIDManagerScheduleWithRunLoop(manager_, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
-
-        IOReturn result = IOHIDManagerOpen(manager_, kIOHIDOptionsTypeSeizeDevice);
-        if (result == kIOReturnSuccess) {
-          std::cout << "opened" << std::endl;
-          IOHIDManagerRegisterInputValueCallback(manager_, inputValueCallback, this);
-        }
-      }
-    }
-  }
-
-  ~IOHIDManagerObserver(void) {
-    if (manager_) {
-      IOHIDManagerUnscheduleFromRunLoop(manager_, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
-      CFRelease(manager_);
-      manager_ = nullptr;
-    }
-  }
-
-private:
-  CFDictionaryRef createDeviceMatchingDictionary(uint32_t inUsagePage, uint32_t inUsage) {
-    auto deviceMatchingDictionary = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
-    if (!deviceMatchingDictionary) {
-      goto finish;
-    }
-
-    // inUsagePage
-    if (!inUsagePage) {
-      goto finish;
-    } else {
-      auto usagePage = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &inUsagePage);
-      if (!usagePage) {
-        goto finish;
-      }
-      CFDictionarySetValue(deviceMatchingDictionary, CFSTR(kIOHIDDeviceUsagePageKey), usagePage);
-      CFRelease(usagePage);
-    }
-
-    // inUsage (The usage is only valid if the usage page is also defined)
-    if (!inUsage) {
-      goto finish;
-    } else {
-      auto usage = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &inUsage);
-      if (!usage) {
-        goto finish;
-      }
-      CFDictionarySetValue(deviceMatchingDictionary, CFSTR(kIOHIDDeviceUsageKey), usage);
-      CFRelease(usage);
-    }
-
-  finish:
-    return deviceMatchingDictionary;
-  }
-
-  CFArrayRef createDeviceMatchingDictionaryArray(void) {
-    auto deviceMatchingDictionaryArray = CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks);
-    if (!deviceMatchingDictionaryArray) {
-      return nullptr;
-    }
-
-    // kHIDUsage_GD_Keyboard
-    {
-      auto deviceMatchingDictionary = createDeviceMatchingDictionary(kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard);
-      if (deviceMatchingDictionary) {
-        CFArrayAppendValue(deviceMatchingDictionaryArray, deviceMatchingDictionary);
-        CFRelease(deviceMatchingDictionary);
-      }
-    }
-
-    // kHIDUsage_GD_Mouse
-    {
-      auto deviceMatchingDictionary = createDeviceMatchingDictionary(kHIDPage_GenericDesktop, kHIDUsage_GD_Mouse);
-      if (deviceMatchingDictionary) {
-        CFArrayAppendValue(deviceMatchingDictionaryArray, deviceMatchingDictionary);
-        CFRelease(deviceMatchingDictionary);
-      }
-    }
-
-    return deviceMatchingDictionaryArray;
-  }
-
-  bool getLongProperty(const IOHIDDeviceRef device, const CFStringRef key, long& value) {
-    auto property = IOHIDDeviceGetProperty(device, key);
-    if (property) {
-      if (CFNumberGetTypeID() == CFGetTypeID(property)) {
-        return CFNumberGetValue(static_cast<CFNumberRef>(property), kCFNumberLongType, &value);
-      }
-    }
-    return false;
-  }
-
-  long getVendorID(IOHIDDeviceRef device) {
-    long value = 0;
-    getLongProperty(device, CFSTR(kIOHIDVendorIDKey), value);
-    return value;
-  }
-
-  long getProductID(IOHIDDeviceRef device) {
-    long value = 0;
-    getLongProperty(device, CFSTR(kIOHIDProductIDKey), value);
-    return value;
-  }
-
-  static void inputValueCallback(
-      void* _Nullable context,
-      IOReturn result,
-      void* _Nullable sender,
-      IOHIDValueRef value) {
-    if (!context) {
-      return;
-    }
-
-    IOHIDManagerObserver* self = static_cast<IOHIDManagerObserver*>(context);
-
-    if (value) {
-      auto element = IOHIDValueGetElement(value);
-      auto usagePage = IOHIDElementGetUsagePage(element);
-      auto usage = IOHIDElementGetUsage(element);
-      auto integerValue = IOHIDValueGetIntegerValue(value);
-
-      switch (usagePage) {
-      case kHIDPage_KeyboardOrKeypad:
-        if (usage == kHIDUsage_KeyboardErrorRollOver ||
-            usage == kHIDUsage_KeyboardPOSTFail ||
-            usage == kHIDUsage_KeyboardErrorUndefined ||
-            usage >= kHIDUsage_GD_Reserved) {
-          // do nothing
-        } else {
-          bool keyDown = (integerValue == 1);
-          std::cout << "inputValueCallback usagePage:" << usagePage << " usage:" << usage << " value:" << integerValue << std::endl;
-          if (usage == kHIDUsage_KeyboardCapsLock) {
-            std::cout << "post" << std::endl;
-            (self->postEventWrapper_).postAuxKey(NX_KEYTYPE_MUTE, keyDown);
-            //(self->postEventWrapper_).postKey(0x33, keyDown);
-          }
-          if (usage == kHIDUsage_KeyboardEscape) {
-            exit(0);
-          }
-        }
-        break;
-
-      default:
-        std::cout << "inputValueCallback unknown usagePage:" << usagePage << " usage:" << usage << std::endl;
-      }
-    }
-  }
-
-  IOHIDManagerRef manager_;
-  IOHIDPostEventWrapper postEventWrapper_;
-};
-
 class EventHook {
 public:
   EventHook(void) : eventTap_(nullptr) {
@@ -294,7 +135,7 @@ private:
 
 int main(int argc, const char* argv[]) {
   //IOHIDQueueInterfaceHook hook1;
-  IOHIDManagerObserver observer;
+  event_grabber observer;
   //EventHook hook2;
   CFRunLoopRun();
   return 0;
