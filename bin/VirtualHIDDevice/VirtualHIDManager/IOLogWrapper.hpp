74f7e97b18fb89447bbb91a6bbfe6ce3eef69d0c
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-09 21:50:40 +0900

    update

--- bin/VirtualHIDDevice/VirtualHIDManager/IOLogWrapper.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define IOLOG_INFO(...)                                               \
-  {                                                                   \
-    IOLog("org.pqrs.driver.VirtualHIDManager --Info-- " __VA_ARGS__); \
-  }
--- bin/VirtualHIDDevice/scripts/load.sh
+++ bin/VirtualHIDDevice/scripts/load.sh
@@ -3,13 +3,13 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
 
 # --------------------------------------------------
-targetdir=/tmp/org.pqrs.driver.VirtualHIDManager
+targetdir=/tmp/org.pqrs.driver.VirtualHIDKeyboard
 
 sudo rm -rf $targetdir
 mkdir $targetdir
 
-cp -R build/Release/VirtualHIDManager.kext $targetdir/VirtualHIDManager.signed.kext
+cp -R build/Release/VirtualHIDKeyboard.kext $targetdir/VirtualHIDKeyboard.signed.kext
 bash ../../script/codesign.sh $targetdir
 sudo chown -R root:wheel $targetdir
 
-sudo kextutil -t $targetdir/VirtualHIDManager.signed.kext
+sudo kextutil -t $targetdir/VirtualHIDKeyboard.signed.kext
