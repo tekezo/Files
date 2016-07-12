68eb884e369ccf985a0caa313441e47a73fba2b3
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-12 13:01:06 +0900

    move files to Karabiner-Elements

--- bin/VirtualHIDDevice/scripts/unload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
-
-# --------------------------------------------------
-sudo kextunload -b org.pqrs.driver.VirtualHIDKeyboard
-
-exit 0
