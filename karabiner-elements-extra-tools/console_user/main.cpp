242cacf07da9f290f853c2038eede436580e2c6a
Takayama Fumihiko <tekezo@pqrs.org>
2019-08-18 11:36:46 +0900

    add comments

--- karabiner-elements-extra-tools/console_user/main.cpp
+++ karabiner-elements-extra-tools/console_user/main.cpp
@@ -1,3 +1,8 @@
+// How to build and run this program.
+//
+// $ c++ -std=c++17 main.cpp -framework SystemConfiguration -framework CoreFoundation
+// $ ./a.out
+
 #include <SystemConfiguration/SystemConfiguration.h>
 #include <iostream>
 
