944ab09b244193674821374b8b9b44e3e7207052
Takayama Fumihiko <tekezo@pqrs.org>
2019-08-18 09:11:51 +0900

    add karabiner-elements-extra-tools/console_user

---
+++ karabiner-elements-extra-tools/console_user/main.cpp
@@ -0,0 +1,17 @@
+#include <SystemConfiguration/SystemConfiguration.h>
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+  uid_t uid = 0;
+  gid_t gid = 0;
+
+  if (auto cf_name = SCDynamicStoreCopyConsoleUser(nullptr, &uid, &gid)) {
+    std::cout << "uid: " << uid << std::endl;
+    std::cout << "gid: " << gid << std::endl;
+    CFRelease(cf_name);
+  } else {
+    std::cout << "SCDynamicStoreCopyConsoleUser returns nullptr" << std::endl;
+  }
+
+  return 0;
+}
