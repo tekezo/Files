ecb76246dfba4ab6f5d54882e4503d22b858ba8b
Takayama Fumihiko <tekezo@pqrs.org>
2019-11-10 09:18:09 +0900

    update script/unmount_other_macos_volumes.sh

--- script/unmount_other_macos_volumes.sh
+++ script/unmount_other_macos_volumes.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-find /Volumes/macos_* -type d -maxdepth 0 -print0 | xargs -0 diskutil unmount
+find /Volumes/macos_* -type d -maxdepth 0 | while read v; do
+  diskutil unmount "$v"
+done
