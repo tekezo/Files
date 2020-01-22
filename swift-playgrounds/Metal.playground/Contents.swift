cf32346a421d2305718392d7420ee0350507b458
Takayama Fumihiko <tekezo@pqrs.org>
2020-01-22 09:03:26 +0900

    update UI

--- swift-playgrounds/Metal.playground/Contents.swift
+++ swift-playgrounds/Metal.playground/Contents.swift
@@ -7,16 +7,18 @@ let source = """
 #include <metal_stdlib>
 using namespace metal;
 
-float3 circle(float2 uv, float r, float blur, float3 color) {
+float3 circle(float2 uv, float r, float blur, float3 color)
+{
     float d = length(uv);
     float c = smoothstep(r, r - blur, d);
     return float3(color * c);
 }
 
 kernel void focusedWindowChangedEffect(texture2d<float, access::write> o[[texture(0)]],
-                                       constant float &time [[buffer(0)]],
-                                       constant float3 &color [[buffer(1)]],
-                                       ushort2 gid [[thread_position_in_grid]]) {
+                                       constant float &time[[buffer(0)]],
+                                       constant float3 &color[[buffer(1)]],
+                                       ushort2 gid[[thread_position_in_grid]])
+{
     float width = o.get_width();
     float height = o.get_height();
 
@@ -51,30 +53,30 @@ public class MetalViewRenderer: NSObject, MTKViewDelegate {
         let library = try! device.makeLibrary(source: source, options: nil)
         let function = library.makeFunction(name: "focusedWindowChangedEffect")!
         cps = try! device.makeComputePipelineState(function: function)
-
+        
         super.init()
         view.delegate = self
         view.device = device
     }
-
+    
     public func mtkView(_: MTKView, drawableSizeWillChange _: CGSize) {}
-
+    
     public func draw(in view: MTKView) {
         var time = Float(Date().timeIntervalSince(startDate))
-
+        
         if time > 1.0 {
             view.isPaused = true
         }
-
+        
         if let drawable = view.currentDrawable,
             let commandBuffer = commandQueue.makeCommandBuffer(),
             let commandEncoder = commandBuffer.makeComputeCommandEncoder() {
             commandEncoder.setComputePipelineState(cps)
             commandEncoder.setTexture(drawable.texture, index: 0)
-
+            
             commandEncoder.setBytes(&time, length: MemoryLayout<Float>.size * 1, index: 0)
             commandEncoder.setBytes(&color, length: MemoryLayout<Float>.size * 3, index: 1)
-
+            
             let w = cps.threadExecutionWidth
             let h = cps.maxTotalThreadsPerThreadgroup / w
             let threadsPerThreadgroup = MTLSize(width: w, height: h, depth: 1)
@@ -126,25 +128,32 @@ struct ContentView: View {
                 minWidth: 200,
                 minHeight: 200
             )
-            
-            HStack {
-                Button(action:{
-                    delegate?.setColor(vector_float3(1.0, 0.3, 0.2))
-                }) {
-                    Text("Red")
-                }
-                Button(action:{
-                    delegate?.setColor(vector_float3(0.2, 1.0, 0.3))
-                }) {
-                    Text("Green")
-                }
-                Button(action:{
-                    delegate?.setColor(vector_float3(0.3, 0.2, 1.0))
-                }) {
-                    Text("Blue")
+
+            Divider()
+
+            VStack {
+                Text("Choose effect color")
+                HStack {
+                    Button(action:{
+                        delegate?.setColor(vector_float3(1.0, 0.3, 0.2))
+                    }) {
+                        Text("Red")
+                    }
+                    Button(action:{
+                        delegate?.setColor(vector_float3(0.2, 1.0, 0.3))
+                    }) {
+                        Text("Green")
+                    }
+                    Button(action:{
+                        delegate?.setColor(vector_float3(0.3, 0.2, 1.0))
+                    }) {
+                        Text("Blue")
+                    }
                 }
             }
             
+            Divider()
+
             Button(action:{
                 delegate?.restart()
             }) {
